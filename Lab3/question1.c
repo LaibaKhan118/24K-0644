72b106ba13faf1bfa04b0e6a5cc293a79f37dd9c
Laiba Khan <80578072+LaibaKhan118@users.noreply.github.com>
2024-09-06 02:48:38 +0500

    Added Explanation

--- Lab3/question1.c
+++ Lab3/question1.c
@@ -1,4 +1,8 @@
-// Because the added number was out of the range of the variable, we are getting a different value.
+/* 
+ANSWER:
+Because the given number was out of the range of the variable, we are getting a different value than the input value.
+If we increase the size of the variable then we'll be able to save and print this huge number. 
+*/
 
 // Here is the correct code
 #include <stdio.h>
