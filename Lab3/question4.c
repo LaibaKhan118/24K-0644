3559f9a77b3edb6fee934c9651835f3af03cdee0
Laiba Khan <80578072+LaibaKhan118@users.noreply.github.com>
2024-09-06 02:34:09 +0500

    Added a comment

--- Lab3/question4.c
+++ Lab3/question4.c
@@ -5,6 +5,7 @@ int main()
 	int fuel_avg, forward_fuel, return_fuel, fuel_used, total_fuel;
 	printf("Enter you car's fuel average (km/liter): \t");
 	scanf("%d", &fuel_avg);
+	// We could've used if-else statement but it would only check once, and we won't be able to check if the user entered the wrong number again. 
 	while (fuel_avg <=0) {
 		printf("Please Enter a positive number that is greater than zero:\t");
 		scanf("%d", &fuel_avg);
