384f05e9d27f4873b09679fc37245ba7dbca10bd
Laiba Khan <80578072+LaibaKhan118@users.noreply.github.com>
2024-10-04 13:27:56 +0500

    Update Q5.c

--- Lab_6/Q5.c
+++ Lab_6/Q5.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
-int main() {
-	int i;
-	for(i = 1; i <= 6; i++){
-		if(i == 1 || i == 6)
-			printf("* * * * *\n");
-		else
-			printf("*       *\n");
-	}
+int main()
+{
+    int i;
+    for(i = 1; i <= 30; i++) {
+        if((i<=5 || i>=26) || (i%5==1)) {
+              printf("* ");
+           }
+       else if(i%5!=0)
+             printf("  ");
+       else 
+             printf("*");
+        if(i%5==0)
+           printf("\n");
+    }
 }
+// time wasted: 5 hours
