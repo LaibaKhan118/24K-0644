18d52e4ec0e3d77b6983800381e146851d3667cf
Laiba Khan <80578072+LaibaKhan118@users.noreply.github.com>
2024-10-07 17:52:48 +0500

    Update Q6.c

--- Lab_6/Q6.c
+++ Lab_6/Q6.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
-int main() {
-	int i;
-	for(i = 1; i <= 6; i++){
-		if(i == 1 || i == 6)
-			printf("*       *\n");
-		else
-			printf("* * * * *\n");
-	}
+int main()
+{
+    int i;
+    for(i = 1; i <= 30; i++) {
+        if((i > 1 && i < 5) || (i > 26 && i < 30)){
+              printf("  ");
+           }
+       else 
+             printf("* ");
+        if(i % 5 == 0)
+           printf("\n");
+    }
 }
