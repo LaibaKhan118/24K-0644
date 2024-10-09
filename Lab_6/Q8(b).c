36d0c539e89f9e6050240a7f30aafb45637ddd05
Laiba Khan <80578072+LaibaKhan118@users.noreply.github.com>
2024-10-09 19:18:30 +0500

    Update Q8(b).c

--- Lab_6/Q8(b).c
+++ Lab_6/Q8(b).c
@@ -1,12 +1,23 @@
 #include<stdio.h>
 int main() {
-	int i;
-	for(i = 1; i <= 5; i++){
-		if(i == 1 || i == 5)
-			printf("* * * * *\n");
-		else if(i == 2 || i == 4)
-			printf("  *   *  \n");
-		else
-			printf("    *    \n");	
-	}
+	int i, num = 25;
+	for(i = 1; i <= num; i++) {
+		if(i <= 5 || i >= 21) {
+			printf("* ");
+		}
+		else if(((i >= 6 && i <= 10) || (i >= 16 && i <= 20)) && (i % 5 == 2 || i % 5 == 4)) {
+            			printf("* ");
+        	}
+        	else if(i >= 11 && i <= 15 && (i % 5 == 3)) {
+            		printf("* ");
+        	}
+        	else {
+            		printf("  ");
+        	}
+        	if(i % 5 == 0) {
+            		printf("\n");
+        	}
+    	}
+    	return 0;
 }
+
