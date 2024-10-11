0dbc4bff56b428d8abfb183db37621180bf89f95
Laiba Khan <80578072+LaibaKhan118@users.noreply.github.com>
2024-10-11 00:04:22 -0700

    Create Q3.c

---
+++ Lab_7/Q3.c
@@ -0,0 +1,35 @@
+/*Q: Write a program that calculates the length of a string using a pointer. Do not use the
+built-in strlen() or sizeof() function. After length calculation reverse the string using
+the same pointers*/
+
+#include <stdio.h>
+#include<string.h>
+int main() {
+    char str[500];
+    printf("Enter a string:\t");
+    gets(str);
+    char *ptr = str;
+    int len = 0;
+    char temp, *start, *end;
+    
+    while(*ptr != '\0') {
+        len++;
+        ptr++;
+    }
+    
+    printf("String: %s\nLength: %d\n", str, len);
+    
+    start = str;
+    end = str + len - 1;
+    
+    while(start < end) {
+        temp = *start;
+        *start = *end;
+        *end = temp;
+        start++;
+        end--;
+    }
+    
+    printf("Reveresed string: %s", str);
+    return 0;
+}
