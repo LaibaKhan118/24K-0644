a003bc0ac159f622c15fb6f9f8eb50110736d4fd
Laiba Khan <80578072+LaibaKhan118@users.noreply.github.com>
2024-10-11 00:14:19 -0700

    Create Q4.c

---
+++ Lab_7/Q4.c
@@ -0,0 +1,34 @@
+/*Q: Write a program which identifies if a given string is a palindrome using arrays.*/
+
+#include <stdio.h>
+
+int main() {
+    char str[500];
+    printf("Enter a string:\t");
+    gets(str);
+    char *ptr = str;
+    int len = 0, palindrome = 0;
+    
+    while(*ptr != '\0') {
+        len++;
+        ptr++;
+    }
+    
+    for(int i = 0; i < len/2; i++) {
+        if(str[i] = str[len-i-1]){
+            palindrome = 1;
+        }
+        else{
+            palindrome = 0;
+            break;
+        }
+    }
+    
+    if (palindrome == 1) {
+        printf("This string is a palindrome\n");
+    }
+    else {
+       printf("This string is not a palindrome\n"); 
+    }
+    return 0;
+}
