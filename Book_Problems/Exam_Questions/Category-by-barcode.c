4a9686a519e82fe5b494105fb21e5f552b7b44ac
Laiba Khan <80578072+LaibaKhan118@users.noreply.github.com>
2024-10-16 09:25:00 -0700

    Create Category-by-barcode.c

---
+++ Book_Problems/Exam_Questions/Category-by-barcode.c
@@ -0,0 +1,27 @@
+//  Assign  categry number to a product by barcode
+
+#include <stdio.h>
+
+int main() {
+    int code, d1, d2, d4, d5;
+    
+    printf("Enter the Barcode:\t");
+    scanf("%d", &code);
+    if (code > 99999 || code <= 9999) {
+        printf("Bar code must b 5 digit long\n");
+        return 0;
+    }
+    
+    d1 = code / 10000;
+    d2 = (code / 1000) % 10;
+    d4 = (code / 10) % 10;
+    d5 = code % 10;
+    
+    if (((d1 % 2 == 0) && (d2 % 2 == 0)) || ((d1 % 4 == 0) && (d2 % 4 == 0)))
+        printf("Category 1");
+    else if (d5 % 3 == 0 && d4 % 2 != 0)
+        printf("Category 2");
+    else 
+        printf("Category 3");
+    return 0;
+}
