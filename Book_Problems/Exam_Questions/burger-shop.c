88caa6cd9b6017870e0765c48447b7bef1d81d71
Laiba Khan <80578072+LaibaKhan118@users.noreply.github.com>
2024-10-16 16:00:10 -0700

    Create burger-shop.c

---
+++ Book_Problems/Exam_Questions/burger-shop.c
@@ -0,0 +1,17 @@
+// 2022 mid B, Q2
+// A burger shop has princple that nothing goes to waste. I they xannot utelize all the available tomatoes and cheese they won't make burgers.
+// a jumbo take 4 t and 1 c ,,,, a small takes 2 t an 1 c
+// for a gven number of tomatoes and cheese, check how many jumbos and smalls can be made ih nothing going to waste. Otherwise print not possible
+#include <stdio.h>
+
+int main() {
+    int j_tom = 4, s_tom = 2, cheese = 1;
+    int tom, che, jumbo = 0, small = 0;
+    
+    printf("Enter the number of Tomatoe Slices available:\t");
+    scanf("%d", &tom);
+    printf("Enter the number of Cheese Slices available:\t");
+    scanf("%d", &che);
+    }
+    return 0;
+}
