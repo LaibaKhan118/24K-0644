34333f423292653c1aee78ede731956865d7ea62
Laiba Khan <80578072+LaibaKhan118@users.noreply.github.com>
2024-10-20 02:34:42 -0700

    Added Input

--- Lab_7/Q1.c
+++ Lab_7/Q1.c
@@ -1,14 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int arr[6] = {1,2,3,4,5,6};
+    int arr[6];
+    for (int i = 0; i < 6; i++)
+    {
+        printf("Enter element %d:\t", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\n\nEntered Array: { ");
+    for (int i = 0; i < 6; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("}\n");
+
     int temp;
-    
-    for(int i = 0; i < 6; i++) {
-    	temp = arr[i];
-    	arr[i] = arr[6 - 1];
-    	arr[6 - 1] = temp;
-    
-		printf("%d", arr[i]);
-	}
+
+    printf("Shifted Array: { ");
+    for (int i = 0; i < 6; i++)
+    {
+        temp = arr[i];
+        arr[i] = arr[6 - 1];
+        arr[6 - 1] = temp;
+
+        printf("%d ", arr[i]);
+    }
+    printf("}\n");
 }
